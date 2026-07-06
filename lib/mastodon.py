27f3e4563cc865be5c7bf14d9485d7aba36aebd9
Jakub Wilk <jwilk@jwilk.net>
2026-07-06 06:58:06 +0200

    Mastodon: add alternative URL template.
    Fixes:
    
        $ zygolophodon https://physics.social/statuses/116849139419647222
        ...
        Traceback (most recent call last):
          ...
        lib.utils.TemplateVarError: cannot expand USER in template '/@USER/IDENT'; expected str instance, NoneType found
    
    https://github.com/jwilk/zygolophodon/issues/16

--- lib/mastodon.py
+++ lib/mastodon.py
@@ -283,6 +283,7 @@ class Mastodon(Mastodonoid):
     tag_url_template = '/tags/TAG'
 
     post_url_template = '/@USER/IDENT'
+    alt_post_url_template = '/statuses/IDENT'
 
     post_id_regexp = '[0-9]{1,18}'
     # Source: lib/mastodon/snowflake.rb
